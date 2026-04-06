a5a3a09685706ea64ae658c14cda02fcb025a787
cimess <cimessthemanofvalor@gmail.com>
2026-04-06 15:41:30 +0100

    added honeycomb

--- backend/tracing.ts
+++ backend/tracing.ts
@@ -1,4 +1,5 @@
 import { metrics, diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import 'dotenv/config';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
